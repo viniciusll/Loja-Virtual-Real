d8f489956c58947ca3cc06e7d3711efb50d1c446
ViniciusLemosM <40804611+viniciusll@users.noreply.github.com>
2021-02-05 22:17:19 -0300

    responsive

--- src/components/Page/styles.ts
+++ src/components/Page/styles.ts
@@ -3,16 +3,74 @@ import styled from 'styled-components'
 export const Container = styled.div`
   .colored:nth-child(1) {
     background-image: url("images/s21.png");
-    /* background-repeat: no-repeat; */
-    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: 100%;
+
+    @media (min-width: 200px) and (max-width: 500px) {
+      background-image: url("images/s21-Mobile-3.png");
+      background-size: cover;
+      background-attachment: fixed;
+    }
+
+    @media (min-width: 501px) and (max-width: 800px) {
+      background-image: url("images/s21-Mobile.png");
+      background-size: 100% 100%;
+      background-attachment: fixed;
+    }
+
+    @media (min-width: 801px) {
+      background-image: url("images/s21.png");
+      background-size: 100% 100%;
+      background-attachment: fixed;
+    }
   }
   .colored:nth-child(2) {
     background-image: url("images/s20.png");
     background-size: cover;
+    background-attachment: fixed;
+
+    @media (min-width: 200px) and (max-width: 500px) {
+      background-image: url("images/s20-Mobile-2.png");
+      background-size: 100% 100%;
+      background-attachment: fixed;
+    }
+
+    @media (min-width: 501px) and (max-width: 800px) {
+      background-image: url("images/s20-Mobile.png");
+      background-size: 100% 100%;
+      background-attachment: fixed;
+    }
+
+    @media (min-width: 801px) {
+      background-image: url("images/s20.png");
+      background-size: 100% 100%;
+      background-attachment: fixed;
+    }
   }
   .colored:nth-child(3) {
     background-image: url("images/s20Fe.png");
     background-size: cover;
+    background-attachment: fixed;
+
+    @media (min-width: 200px) and (max-width: 500px) {
+      background-image: url("images/s20Fe-Mobile-2.png");
+      background-size: cover;
+      background-attachment: fixed;
+    }
+
+    @media (min-width: 501px) and (max-width: 800px) {
+      background-image: url("images/s20Fe-Mobile.png");
+      background-size: 100% 100%;
+      background-attachment: fixed;
+    }
+
+    @media (min-width: 801px) {
+      background-image: url("images/s20Fe.png");
+      background-size: 100% 100%;
+      background-attachment: fixed;
+    }
+
   }
   /* .colored:nth-child(4) {
     background: #bbfebb;
